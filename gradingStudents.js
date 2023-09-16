f8a2fd1fcfc122cb643970144bee76206dbc6e56
Adesh <shaha@algonquincollege.com>
2023-09-16 14:35:56 -0400

    Solution for first test

---
+++ gradingStudents.js
@@ -0,0 +1,13 @@
+// Case: Grading Students
+
+// You are a teacher, and you want to calculate and grade your students' test scores. You want to teach your students how to use JavaScript to determine their grades based on their scores.
+
+// Here are the grading criteria:
+
+// A score of 90 or above is an 'A.'
+// A score between 80 and 89 is a 'B.'
+// A score between 70 and 79 is a 'C.'
+// A score between 60 and 69 is a 'D.'
+// Any score below 60 is an 'F.'
+
+// Your task is to write a JavaScript program that takes a student's score and calculates their grade. Additionally, you want to create a loop to calculate grades for multiple students.
